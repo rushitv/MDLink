458b6eb87b736088cce02723148d50bbe3cadace
Rushit Vaishnani <>
2018-10-22 23:42:03 +0530

    util added

--- app/src/main/java/com/mdlink/util/ValidationsUtil.java
+++ app/src/main/java/com/mdlink/util/ValidationsUtil.java
@@ -5,4 +5,7 @@ public class ValidationsUtil {
     public static boolean isPasswordValid(String nPwd, String cPwd){
         return (nPwd == cPwd);
     }
+    public static String getPaddedNumber(int number) {
+        return String.format("%02d", number);
+    }
 }
